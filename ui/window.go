1dd27f456cf2d735d5027e294007b0d270181b0b
LeVasTiaN <81701123+LeVasTiaN@users.noreply.github.com>
2025-06-15 14:58:58 +0200

    Implement T-90 figure drawing

--- ui/window.go
+++ ui/window.go
@@ -27,6 +27,8 @@ type Visualizer struct {
 
 	sz  size.Event
 	pos image.Rectangle
+
+	FigurePos image.Rectangle
 }
 
 func (pw *Visualizer) Main() {
@@ -34,80 +36,48 @@ func (pw *Visualizer) Main() {
 	pw.done = make(chan struct{})
 	pw.pos.Max.X = 200
 	pw.pos.Max.Y = 200
+
+	pw.FigurePos = image.Rect(300, 300, 550, 500)
+
 	driver.Main(pw.run)
 }
 
-func (pw *Visualizer) Update(t screen.Texture) {
-	pw.tx <- t
-}
+func (pw *Visualizer) drawDefaultUI() {
+	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src)
 
-func (pw *Visualizer) run(s screen.Screen) {
-	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
-	})
-	if err != nil {
-		log.Fatal("Failed to initialize the app window:", err)
-	}
-	defer func() {
-		w.Release()
-		close(pw.done)
-	}()
-
-	if pw.OnScreenReady != nil {
-		pw.OnScreenReady(s)
+	if pw.FigurePos.Empty() {
+		centerX := 800 / 2
+		centerY := 800 / 2
+		pw.FigurePos = image.Rect(
+			centerX-125, centerY-100,
+			centerX+125, centerY+100,
+		)
 	}
 
-	pw.w = w
+	figureColor := color.RGBA{B: 255, A: 255}
 
-	events := make(chan any)
-	go func() {
-		for {
-			e := w.NextEvent()
-			if pw.Debug {
-				log.Printf("new event: %v", e)
-			}
-			if detectTerminate(e) {
-				close(events)
-				break
-			}
-			events <- e
-		}
-	}()
+	verticalBar := image.Rect(
+		pw.FigurePos.Min.X, pw.FigurePos.Min.Y,
+		pw.FigurePos.Min.X+50, pw.FigurePos.Max.Y,
+	)
 
-	var t screen.Texture
+	horizontalBar := image.Rect(
+		pw.FigurePos.Min.X, pw.FigurePos.Min.Y+75,
+		pw.FigurePos.Max.X, pw.FigurePos.Min.Y+125,
+	)
 
-	for {
-		select {
-		case e, ok := <-events:
-			if !ok {
-				return
-			}
-			pw.handleEvent(e, t)
+	pw.w.Fill(verticalBar, figureColor, draw.Src)
+	pw.w.Fill(horizontalBar, figureColor, draw.Src)
 
-		case t = <-pw.tx:
-			w.Send(paint.Event{})
-		}
+	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
+		pw.w.Fill(br, color.White, draw.Src)
 	}
-}
-
-func detectTerminate(e any) bool {
-	switch e := e.(type) {
-	case lifecycle.Event:
-		if e.To == lifecycle.StageDead {
-			return true // Window destroy initiated.
-		}
-	case key.Event:
-		if e.Code == key.CodeEscape {
-			return true // Esc pressed.
-		}
-	}
-	return false
 }
 
 func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 	switch e := e.(type) {
 
-	case size.Event: // Оновлення даних про розмір вікна.
+	case size.Event:
 		pw.sz = e
 
 	case error:
@@ -119,11 +89,9 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 		}
 
 	case paint.Event:
-		// Малювання контенту вікна.
 		if t == nil {
 			pw.drawDefaultUI()
 		} else {
-			// Використання текстури отриманої через виклик Update.
 			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
 		}
 		pw.w.Publish()
