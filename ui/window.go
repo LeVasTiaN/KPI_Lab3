154cdf6a730885ea5d16ebeda4adc9b11093433e
LeVasTiaN <81701123+LeVasTiaN@users.noreply.github.com>
2025-06-15 15:01:02 +0200

    Add mouse interaction

--- ui/window.go
+++ ui/window.go
@@ -42,6 +42,109 @@ func (pw *Visualizer) Main() {
 	driver.Main(pw.run)
 }
 
+func (pw *Visualizer) Update(t screen.Texture) {
+	pw.tx <- t
+}
+
+func (pw *Visualizer) run(s screen.Screen) {
+	w, err := s.NewWindow(&screen.NewWindowOptions{
+		Title:  pw.Title,
+		Width:  800,
+		Height: 800,
+	})
+	if err != nil {
+		log.Fatal("Failed to initialize the app window:", err)
+	}
+	defer func() {
+		w.Release()
+		close(pw.done)
+	}()
+
+	if pw.OnScreenReady != nil {
+		pw.OnScreenReady(s)
+	}
+
+	pw.w = w
+
+	events := make(chan any)
+	go func() {
+		for {
+			e := w.NextEvent()
+			if pw.Debug {
+				log.Printf("new event: %v", e)
+			}
+			if detectTerminate(e) {
+				close(events)
+				break
+			}
+			events <- e
+		}
+	}()
+
+	var t screen.Texture
+
+	for {
+		select {
+		case e, ok := <-events:
+			if !ok {
+				return
+			}
+			pw.handleEvent(e, t)
+
+		case t = <-pw.tx:
+			w.Send(paint.Event{})
+		}
+	}
+}
+
+func detectTerminate(e any) bool {
+	switch e := e.(type) {
+	case lifecycle.Event:
+		if e.To == lifecycle.StageDead {
+			return true
+		}
+	case key.Event:
+		if e.Code == key.CodeEscape {
+			return true
+		}
+	}
+	return false
+}
+
+func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
+	switch e := e.(type) {
+
+	case size.Event:
+		pw.sz = e
+
+	case error:
+		log.Printf("ERROR: %s", e)
+
+	case mouse.Event:
+		if t == nil {
+			if e.Button == mouse.ButtonLeft && e.Direction == mouse.DirPress {
+				newX := int(e.X)
+				newY := int(e.Y)
+
+				pw.FigurePos = image.Rect(
+					newX-125, newY-100,
+					newX+125, newY+100,
+				)
+
+				pw.w.Send(paint.Event{})
+			}
+		}
+
+	case paint.Event:
+		if t == nil {
+			pw.drawDefaultUI()
+		} else {
+			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
+		}
+		pw.w.Publish()
+	}
+}
+
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src)
 
@@ -69,41 +172,6 @@ func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(verticalBar, figureColor, draw.Src)
 	pw.w.Fill(horizontalBar, figureColor, draw.Src)
 
-	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
-		pw.w.Fill(br, color.White, draw.Src)
-	}
-}
-
-func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
-	switch e := e.(type) {
-
-	case size.Event:
-		pw.sz = e
-
-	case error:
-		log.Printf("ERROR: %s", e)
-
-	case mouse.Event:
-		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
-		}
-
-	case paint.Event:
-		if t == nil {
-			pw.drawDefaultUI()
-		} else {
-			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
-		}
-		pw.w.Publish()
-	}
-}
-
-func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
-
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
-
-	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
 		pw.w.Fill(br, color.White, draw.Src)
 	}
